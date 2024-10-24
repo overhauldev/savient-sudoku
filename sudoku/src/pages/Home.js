a1aa221f7db7662d126f1742ab695f2c38e884a9
overhauldev <archietownsend999@gmail.com>
2024-10-24 11:41:53 +0100

    Refactor CSS to add margin-bottom to App logo

--- sudoku/src/pages/Home.js
+++ sudoku/src/pages/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+
 function Home() {
   return (
     <div className="App-header">
       <h1>Savient Sudoku</h1>
       <p>Hello agents! Complete this puzzle to earn a secret badge!</p>
       <Link to="/Sudoku">
-        <button type="button" class="btn btn-outline-primary btn-lg">
+        <button type="button" className="btn btn-outline-primary btn-lg">
           Start
         </button>
-      </Link>{" "}
+      </Link>
     </div>
   );
 }

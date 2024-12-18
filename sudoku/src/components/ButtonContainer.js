ac0cc945b899a798d320701643ee39ebe2dc14f7
overhauldev <archietownsend999@gmail.com>
2024-12-18 23:41:22 +0000

    Remove Reset button from ButtonContainer component

--- sudoku/src/components/ButtonContainer.js
+++ sudoku/src/components/ButtonContainer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../App.css";
 import InfoModal from "./InfoModal";
 
-const ButtonContainer = ({ onReset, onCheckSolution }) => {
+const ButtonContainer = ({ onCheckSolution }) => {
   const [showInfoModal, setShowInfoModal] = useState(false);
 
   const handleInfoClick = () => {
@@ -16,14 +16,6 @@ const ButtonContainer = ({ onReset, onCheckSolution }) => {
 
   return (
     <div className="Button-container">
-      <button
-        type="button"
-        className="btn btn-outline-primary btn-lg"
-        onClick={onReset}
-      >
-        Reset
-      </button>
-
       <Link to="/">
         <button type="button" className="btn btn-outline-primary btn-lg">
           Home
